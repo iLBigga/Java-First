48b19b12f18a9f5b1f0f6eb32a306a235fc2fb5f
iLBigga <giannanti1996@gmail.com>
2023-10-03 14:35:36 +0200

    Ex5

---
+++ Ex5.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class Ex5 {
+
+
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+
+		// System.out.println("Divisibile per 5");
+		
+		// for(int i = 1; i <= 100; i++){
+		// 	if(i % 5 == 0){ // Divisibile per 5
+		// 		System.out.print(i + ",");
+		// 	}
+
+		// }
+
+		// System.out.println(" ");
+		// System.out.println("Divisibile per 3");
+
+		// for(int i = 1; i <= 100; i++){
+
+		// 	if(i % 3 == 0){ // Divisibile per 3
+		// 		System.out.print(i + ",");
+		// 	}
+		// }
+
+		// System.out.println(" ");
+		// System.out.println("Divisibile per 3 e 5");
+		// for(int i = 1; i <= 100; i++){
+		// 	if((i % 5 == 0) && (i % 3 == 0)){ // Divisibile per 3 e 5
+		// 		System.out.print(i + ",");
+		// 	}
+		// }
+
+		// System.out.println("Inserire un numero");
+		// int a = scan.nextInt();
+
+		// for(int i = 0; i < a; i++){
+		// 	for(int y = i + 1; y < a; y++){
+		// 		for(int z = y + 1; z < a; z++){
+		// 			if(i + y + z == a){
+		// 				System.out.println(i + " + " + y + " + " + z + " = " + a );
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+		// System.out.println("Inserire un numero");
+		// int nRighe = scan.nextInt();
+
+		// for(int i = 1; i <= nRighe; i++){
+		// 	for(int x = 1; x <= i; x++){
+		// 		System.out.print(x);
+		// 		if(x == i) System.out.println(" ");
+		// 	}
+		// }
+
+
+		// System.out.println("Inserire numero tabella");
+		// int t = scan.nextInt();
+
+		// for(int i = 0; i <= t; i++){
+		// 	int x = t * i;
+
+		// 	String text = t + " X " + i + " = " + x;	
+		// 	System.out.println(text);	
+		// }
+
+		// for(int i = 1; i <= 10; i++ ){
+		// 	System.out.print(i);
+		// }
+
+		// System.out.println("Inserire numero");
+		// int n = scan.nextInt();
+
+		// int s = 0;
+
+		// System.out.println(n);
+
+		// for(int i = n; i >= 1; i--){
+		// 	String t = i + ",";
+		// 	if(i == 1) t = i + " ";
+		// 	System.out.print(t);
+		// }
+
+		// System.out.println(" ");
+
+		// for(int i = 1; i <= n; i++){
+		// 	String t = i + " + ";
+		// 	if(i == n) t = i + " = ";
+		// 	System.out.print(t);
+		// 	s = s + i;
+		// 	if(i == n) System.out.print(s);
+		// }
+
+		// System.out.println("Inserire numero");
+		// int n = scan.nextInt();
+
+		// System.out.println(n);
+
+		// for(int i = 1; i <= n; i++){
+		// 	int p = (int)Math.pow(i,i);
+		// 	if(i == n) p = (int)Math.pow(i,i-1);
+		// 	System.out.print("Il numero e': " + i + " e' il cubo di " + i + " e': " + p);
+		// 	System.out.println("");
+		// }
+	}
+}
